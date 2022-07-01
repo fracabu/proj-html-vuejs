c0ae5a8ac36bc5ab5c3154d57534ad3165a60065
fracabu <100199965+fracabu@users.noreply.github.com>
2022-07-01 17:20:06 +0200

    mod

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -5,8 +5,22 @@
         </div>
         <div class="container">
             <div class="row d-flex ">
-            <div class="col-8">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, soluta. Consectetur ex incidunt assumenda nam quasi, mollitia voluptates fugiat aliquam recusandae expedita! Ad aliquid corrupti necessitatibus, explicabo facilis quas deleniti!</div>
-            <div class="col-4"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi pariatur distinctio perferendis rem asperiores sunt qui labore? Doloribus praesentium odit adipisci veritatis dicta molestiae dolore dolores ipsum quasi, qui accusamus!</div>   
+                <div class="col-8">
+                    <h1 class="text-uppecase text start">every moment count</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, quibusdam consectetur quam porro iure magni alias fuga numquam, tempora voluptate id explicabo accusantium ullam aut distinctio modi ipsam praesentium sunt?</p>
+                    <div><button>our mission</button></div>
+                </div>
+                <div class="col-4">
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aut natus odio quisquam, iure
+                        expedita nobis aspernatur facere, harum dicta adipisci repudiandae. Ex debitis ipsa quos, ullam
+                        nam neque illo!</div>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, provident sequi commodi et non
+                        in tempore facere est, consectetur quis quos modi iure vitae vel qui omnis explicabo nulla
+                        facilis!</div>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita possimus sequi est
+                        quibusdam quia! Qui unde incidunt, perferendis, praesentium enim placeat tempore facere
+                        molestias illo eum nam aliquam natus?</div>
+                </div>
             </div>
         </div>
     </div>
@@ -26,7 +40,9 @@ export default {
     right: 0;
     left: 0;
     right: 0;
-};
+}
+
+;
 
 .jumbo-img {
     width: 100%;
@@ -34,15 +50,13 @@ export default {
 
 }
 
-.row{
+.row {
     position: relative;
     margin-top: 238px;
     border: 1px solid black;
 }
 
-.col-4{
+.col-4 {
     background-color: #ED6866;
 }
-
-
 </style>
