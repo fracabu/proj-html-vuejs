22b5ce1cdc468b975f4feebaa6378043a6cf6f2f
fracabu <100199965+fracabu@users.noreply.github.com>
2022-07-01 12:16:42 +0200

    settings

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="jumbotron">
+        <img src="" alt="">
+    </div>
+    <div class="nav">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
